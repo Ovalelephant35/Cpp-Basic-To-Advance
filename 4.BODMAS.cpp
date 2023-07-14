bb3b5b9b025b9ba4551fea11fdad8bebe8fc8fbd
Oval_Elephant35 <principalquantum30@gmail.com>
2023-07-14 22:23:14 +0530

    CPP basics

---
+++ 4.BODMAS.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    // cpp follows bodmas rule
+    int ex = (4+3)*7;
+    int num2 = 4+3*7;
+    cout << ex <<" " << num2<<endl;
+    return 0;}
+
---
+++ 6.Function.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+void print();
+int main(){
+    print();
+    return 0;
+}
+ void print(){
+    cout << "I have been called" << endl;
+ }
