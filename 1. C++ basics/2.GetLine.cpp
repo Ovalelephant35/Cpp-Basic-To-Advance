a9dd456dc5dd7adf3b982b3e93aa3aaec6de66d1
Oval_Elephant35 <principalquantum30@gmail.com>
2023-07-23 01:34:50 +0530

    CPP Basics

---
+++ 1. C++ basics/2.GetLine.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    string str ;
+    cout << "Enter your Name : "<<endl;
+    getline(cin ,str);
+    cout<<"Hello "<< str <<endl;
+    return 0;
+}
